a5e5bc4464c39aacb9647d80d3881cec562eab2c
Joachim Bengtsson <nevyn@spotify.com>
2011-11-01 16:27:28 +0100

    Initial Commit

---
+++ CPUMonster/OMGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OMGAppDelegate.h
+//  CPUMonster
+//
+//  Created by Joachim Bengtsson on 2011-11-01.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OMGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CPUMonster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CPUMonster
+//
+//  Created by Joachim Bengtsson on 2011-11-01.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OMGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([OMGAppDelegate class]));
+	}
+}
